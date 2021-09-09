ae0fb3f5adca389cf72f1e811f50fed7fd6a75f0
Rachel Gregg <rgregg2014@gmail.com>
2021-09-08 21:58:58 -0400

    added HTML routes, started API routes

---
+++ routes/apiroutes.js
@@ -0,0 +1,47 @@
+const db = require("../models");
+
+module.exports = function (app) {
+  //get all info for workouts page
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  });
+  //get info for a range
+  app.get("/api/workouts/range", ({}, res) => {
+    db.Workout.find({})
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  });
+  //submit new completed workouts
+  app.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body)
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  });
+  //update workouts by id
+  app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+      { _id: req.params.id },
+      { exercises: req.body }
+    )
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  });
+};
--- server.js
+++ server.js
@@ -4,20 +4,20 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
-
 const app = express();
 
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
   useNewUrlParser: true,
 });
+
+require("./routes/htmlroutes")(app);
+require("./routes/apiroutes")(app);
 
 app.listen(PORT, () => {
   console.log(`App running at http://localhost:${PORT}/`);
